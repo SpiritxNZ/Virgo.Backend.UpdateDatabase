6e522ef404c6166bf4b141804dc685e2e2b84581
pickledbrill <pickledbrill@gmail.com>
2019-11-27 23:21:38 +1300

    Added basic service

---
+++ DependencyInjection.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Virgo.Backend.Repository.Context;
+using Virgo.Backend.UpdateDatabase.Helper;
+
+namespace Virgo.Backend.UpdateDatabase
+{
+    public class DependencyInjection
+    {
+        public static IServiceProvider ConfigureServices()
+        {
+            ServiceCollection services = new ServiceCollection();
+
+            string mysqlConnectionString = DatabaseSetupHelper.GetDatabaseConnectionString();
+            services.AddDbContext<VirgoDbContext>(builder =>
+            {
+                builder.UseMySQL(mysqlConnectionString);
+            });
+
+            return services.BuildServiceProvider();
+        }
+    }
+}
---
+++ Helper/DatabaseSetupHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Virgo.Backend.UpdateDatabase.Helper
+{
+    public class DatabaseSetupHelper
+    {
+        public static string GetDatabaseConnectionString()
+        {
+            // TODO: Get connection properties from environment variables
+            return "";
+        }
+    }
+}
