d5f4ace598dea9a76034dad67dba329c6cfefc5b
pickledbrill <pickledbrill@gmail.com>
2019-11-27 22:51:39 +1300

    Initial project commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Virgo.Backend.UpdateDatabase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
