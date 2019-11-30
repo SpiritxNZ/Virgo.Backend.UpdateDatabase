f56ac6a5bb91b9d65e03aa06f365ca92a4bf8277
pickledbrill <pickledbrill@gmail.com>
2019-11-30 16:47:38 +1300

    Added parameters to control the service

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using Virgo.Backend.UpdateDatabase.interfaces;
@@ -10,12 +12,47 @@ namespace Virgo.Backend.UpdateDatabase
         private static IServiceProvider services;
         static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Starting Virgo UpdateDatabase service...");
             services = DependencyInjection.ConfigureServices();
-            Console.WriteLine("wow");
 
             var migrationService = services.GetService<IMigrationService>();
-            await migrationService.MigrateAll();
+
+            Dictionary<string, string> parameters = GetParameters(args);
+            try
+            {
+                if (parameters.ContainsKey("--RunMigration") && bool.Parse(parameters["--RunMigration"]))
+                {
+                    Console.WriteLine("Full migration is selected...");
+                    await migrationService.MigrateAll();
+                }
+                else if (parameters.ContainsKey("--RunMigrationTo"))
+                {
+                    Console.WriteLine($"Target migration is selected: {parameters["--RunMigrationTo"]}");
+                    await migrationService.MigrateToTarget(parameters["RunMigrationTo"]);
+                }
+
+
+                if (parameters.ContainsKey("--RunFix") && bool.Parse(parameters["--RunFix"]))
+                {
+                    // TODO: implement database fix
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Virgo UpdateDatabase service encountered the following error: {e.Message}");
+                throw;
+            }
+            Console.WriteLine("Virgo UpdateDatabase service operation finished...");
+        }
+
+        private static Dictionary<string, string> GetParameters(string[] args)
+        {
+            Dictionary<string, string> parameterDictionary = new Dictionary<string, string>();
+            parameterDictionary = args
+                .Select(x => x.Split('='))
+                .ToDictionary(x => x[0], x => x[1]);
+            return parameterDictionary;
         }
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Virgo.Backend.UpdateDatabase
 This project is responsible for updating database schema on the fly.
+
+## Supported parameters
+`--RunMigration=true | false`  
+`--RunMigrationTo="target ID"`  
+`--RunFix=true | false`
+
+## Apply database migration
+- Full Migration  
+  Under the UpdateDatabase project folder, run the command `dotnet run --RunMigration=true`
+- Target Migration  
+  Under the UpdateDatabase project folder, run the command `dotnet run --RunMigrationTo=<Target ID>`. You need to replace the `<Target ID>` with the string value of your migration.
+
+## Run database fix
+Under the UpdateDatabase project folder, run the command `dotnet run --RunFix=true`.
